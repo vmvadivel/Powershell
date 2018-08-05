939d8a954aacfc9e84ef037f993cbd861b04dfda
vmvadivel <vmvadivel@gmail.com>
2018-08-05 21:01:17 +0530

    Initial commit which creates RG & Storage Account

---
+++ 01CreateVM.ps1
@@ -0,0 +1,17 @@
+﻿  #Login into Microsoft Azure
+  Login-AzureRmAccount
+
+  #Create a Resource Group
+  $resourceGroupName = "VadiTestRG"
+  $location = "south india"
+  New-AzureRmResourceGroup -Name $resourceGroupName 
+                           -location $location
+
+  #Create a new storage account  
+  $storageAccountName = "vadistoragetestsi"
+  $storageAccountLocation = "southindia"
+  $storageAccountType = "Standard_LRS"
+  New-AzureRmStorageAccount -Name $storageAccountName `
+                            -Location $storageAccountLocation `
+                            -Type $storageAccountType `
+                            -ResourceGroupName $resourceGroupName
