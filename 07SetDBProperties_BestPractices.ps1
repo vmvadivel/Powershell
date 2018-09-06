4fcdab1b6d149af8a4199bdc24ae3c81a17d2edf
Vadivel Mohanakrishnan <vmvadivel@gmail.com>
2018-09-06 13:36:18 +0530

    added condition for auto close, create/update stat

--- 07SetDBProperties_BestPractices.ps1
+++ 07SetDBProperties_BestPractices.ps1
@@ -1,10 +1,12 @@
-﻿$sqlservername = "DESKTOP-S25NQBT\VADIVEL2017ENT"
+clear
+
+$sqlservername = "YourServerName"
 $sqlquery  = "SELECT name, is_auto_shrink_on, is_auto_close_on, is_auto_create_stats_on, is_auto_update_stats_on FROM sys.databases"
 $dbs = Invoke-Sqlcmd -ServerInstance $sqlservername -Query $sqlquery
 
 try
 {
-    Write-Host " Going to start reconfigure the SQL Settings as per best practices!"
+    Write-Host "[Info] Going to start investigating & reconfigure the SQL Settings (if needbe) as per best practices!" -ForegroundColor Green
 
     # Loop through the database properties
     foreach ($db in $dbs)
@@ -13,6 +15,25 @@ try
         {
             $sqlquery2 = "ALTER DATABASE " + $db.name + " SET AUTO_SHRINK OFF;"
             Invoke-SQLcmd -Query $sqlquery2 -ServerInstance $sqlservername -Database "master" 
+            Write-Host "[Info] Auto Shrink Turned OFF for Database" $db.name -ForegroundColor Green
+        }
+        if ($db.is_auto_close_on -eq $true)
+        {
+            $sqlquery2 = "ALTER DATABASE " + $db.name + " SET AUTO_CLOSE OFF;"
+            Invoke-SQLcmd -Query $sqlquery2 -ServerInstance $sqlservername -Database "master"  
+            Write-Host "[Info] Auto Close Turned OFF for Database" $db.name -ForegroundColor Green
+        }
+        if ($db.is_auto_create_stats_on -eq $false)
+        {
+            $sqlquery2 = "ALTER DATABASE " + $db.name + " SET AUTO_CREATE_STATISTICS ON (INCREMENTAL = ON );"
+            Invoke-SQLcmd -Query $sqlquery2 -ServerInstance $sqlservername -Database "master"
+            Write-Host "[Info] Auto Create Statistics Turned ON for Database" $db.name -ForegroundColor Green
+        }
+            if ($db.is_auto_update_stats_on -eq $false)
+        {
+            $sqlquery2 = "ALTER DATABASE " + $db.name + " SET AUTO_UPDATE_STATISTICS ON;"
+            Invoke-SQLcmd -Query $sqlquery2 -ServerInstance $sqlservername -Database "master"
+            Write-Host "[Info] Auto Update Statistics Turned ON for Database" $db.name -ForegroundColor Green
         }
     }
 }
